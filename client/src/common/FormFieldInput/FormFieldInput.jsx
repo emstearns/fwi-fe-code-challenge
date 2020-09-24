3af3ce01701d757a162575cc61fd4f215020cf70
Emily Stearns <estearns@umich.edu>
2020-09-24 11:58:34 -0400

    Added Formik support, FormFieldInput component

---
+++ client/src/common/FormFieldInput/FormFieldInput.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { TextField } from '@material-ui/core';
+import './FormFieldInput.scss';
+
+const FormFieldInput = (props) => {
+  if (!props.children) {
+    return (
+      <TextField
+        className="form-field"
+        id={props.name}
+        error={!!props.errors}
+        helperText={props.errors}
+        variant="outlined"
+        margin="dense"
+        fullWidth
+        {...props}
+      />
+    );
+  } else {
+    return (
+      <TextField
+        className="form-field"
+        id={props.name}
+        error={!!props.errors}
+        helperText={props.errors}
+        variant="outlined"
+        margin="dense"
+        fullWidth
+        {...props}
+      >
+        {props.children}
+      </TextField>
+    );
+  }
+};
+
+export default FormFieldInput;
